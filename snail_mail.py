31b9db1ed7f2e33ba1dc3435c66850ea9608075c
Milan0419 <balint.milan@diak.szbi-pg.hu>
2025-11-05 10:37:50 +0100

    Update snail_mail.py

--- snail_mail.py
+++ snail_mail.py
@@ -25,9 +25,9 @@ position_of_first_dot_after_the_at = email.find(".", position_of_at)
 
 error_message_no_at = "An email address has to contain a '@' character!"
 error_message_too_many_at = "An email address cannot contain more than one '@' characters!"
-error_message_no_dot = "An email address has to contain at least one '.' character!"
-error_message_no_username = "The username before the '@' character cannot be empty!"
-error_message_no_dot_in_domain = "The domain has to contain at least one '.' character!"
+error_message_no_dot = "An email address has to contain at least one '.' character!" #asd
+error_message_no_username = "The username before the '@' character cannot be empty!" #asd
+error_message_no_dot_in_domain = "The domain has to contain at least one '.' character!" #asd
 error_message_no_server_name = "The domain cannot start with a '.' character!"
 error_message_no_tld = "The top-level domain cannot be empty!"
 error_message_short_tld = "The top-level domain has to be at least two characters long!"
@@ -45,4 +45,10 @@ while is_valid:
     else:
         username = email[:position_of_at]
         domain = email[position_of_at + 1:]
-
+    if number_of_dot_characters == 0:
+        print(error_message_no_dot)
+        break
+    if username == "":
+        print(error_message_no_username)
+    if domain.count(".") == 0:
+        print(error_message_no_dot_in_domain)
