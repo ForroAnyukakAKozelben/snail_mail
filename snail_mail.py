d639a0beed5831806b61c02095fdc3585a6fd971
Hot Dominik <forro.dominik.adrian@diak.szbi-pg.hu>
2025-11-12 07:23:14 +0100

    Update snail_mail.py

--- snail_mail.py
+++ snail_mail.py
@@ -1,63 +1,59 @@
+while True:
+    email = input("Add meg az email címet: ")
 
+    number_of_at_characters = email.count("@")
+    number_of_dot_characters = email.count(".")
+    position_of_at = email.find("@")
+    position_of_last_dot = email.rfind(".")
 
-number_of_at_characters = email.count("@")
-number_of_dot_characters = email.count(".")
-position_of_at = email.find("@")
-position_of_last_dot = email.rfind(".")
+    error_message_no_at = "Hiba: hiányzik az '@' jel."
+    error_message_too_many_at = "Hiba: túl sok '@' jel."
+    error_message_no_dot = "Hiba: hiányzik a pont ('.')."
+    error_message_no_username = "Hiba: nincs felhasználónév az '@' előtt."
+    error_message_no_domain = "Hiba: nincs domain az '@' után."
+    error_message_no_dot_in_domain = "Hiba: a domainben nincs pont."
+    error_message_no_server_name = "Hiba: a domain ponttal kezdődik."
+    error_message_no_tld = "Hiba: nincs TLD a domain végén."
+    error_message_short_tld = "Hiba: a TLD túl rövid."
+    error_message_invalid_username = "Hiba: a felhasználónév ponttal kezdődik."
+    ok_message = "Valid email address :)"
 
-error_message_no_at = "Hiba: hiányzik az '@' jel."
-error_message_too_many_at = "Hiba: túl sok '@' jel."
-error_message_no_dot = "Hiba: hiányzik a pont ('.')."
-error_message_no_username = "Hiba: nincs felhasználónév az '@' előtt."
-error_message_no_domain = "Hiba: nincs domain az '@' után."
-error_message_no_dot_in_domain = "Hiba: a domainben nincs pont."
-error_message_no_server_name = "Hiba: a domain ponttal kezdődik."
-error_message_no_tld = "Hiba: nincs TLD a domain végén."
-error_message_short_tld = "Hiba: a TLD túl rövid."
-error_message_invalid_username = "Hiba: a felhasználónév ponttal kezdődik."
-ok_message = "Valid email address :)"
+    if number_of_at_characters == 0:
+        print(error_message_no_at)
+        continue
+    elif number_of_at_characters > 1:
+        print(error_message_too_many_at)
+        continue
 
-is_valid = True
-while is_valid
-    while True:
-        if number_of_at_characters == 0:
-            print(error_message_no_at)
-            break
-        elif number_of_at_characters > 1:
-            print(error_message_too_many_at)
-            break
+    username = email[:position_of_at]
+    domain = email[position_of_at + 1:]
 
-        username = email[:position_of_at]
-        domain = email[position_of_at + 1:]
+    if number_of_dot_characters == 0:
+        print(error_message_no_dot)
+        continue
+    if username == "":
+        print(error_message_no_username)
+        continue
+    if domain == "":
+        print(error_message_no_domain)
+        continue
+    if domain.count(".") == 0:
+        print(error_message_no_dot_in_domain)
+        continue
+    if domain[0] == ".":
+        print(error_message_no_server_name)
+        continue
 
-        if number_of_dot_characters == 0:
-            print(error_message_no_dot)
-            break
-        if username == "":
-            print(error_message_no_username)
-            break
-        if domain == "":
-            print(error_message_no_domain)
-            break
-        if domain.count(".") == 0:
-            print(error_message_no_dot_in_domain)
-            break
-        if domain[0] == ".":
-            print(error_message_no_server_name)
-            break
+    tld = domain[domain.rfind(".") + 1:]
+    if tld == "":
+        print(error_message_no_tld)
+        continue
+    if len(tld) <= 2:
+        print(error_message_short_tld)
+        continue
+    if username[0] == ".":
+        print(error_message_invalid_username)
+        continue
 
-        tld = domain[position_of_last_dot + 1:]
-        if tld == "":
-            print(error_message_no_tld)
-            break
-        if len(tld) <= 2:
-            print(error_message_short_tld)
-            break
-        if username[0] == ".":
-            print(error_message_invalid_username)
-            break
-        is_valid = False
-        break
-
-print(ok_message)
-
+    print(ok_message)
+    break
